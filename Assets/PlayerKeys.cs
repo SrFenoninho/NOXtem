11464bb0e9d533e2f8314bf007947c5b141b231b
SrFenoninho <joaogdmr@gmail.com>
2025-12-02 14:14:16 +0000

    Add cuboid object and tweak scene lighting

--- Assets/PlayerKeys.cs
+++ Assets/PlayerKeys.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections.Generic; // necessary for using lists
 
 public class PlayerKeys : MonoBehaviour
 {
-    private List<string> keys = new List<string>();
+    private List<string> keys = new List<string>(); // list makes a list of strings
 
-    public void AddKey(string keyID)
+    public void AddKey(string keyID) // add a key to the player's inventory
     {
         if (!keys.Contains(keyID))
         {
@@ -13,12 +13,12 @@ public class PlayerKeys : MonoBehaviour
         }
     }
 
-    public bool HasKey(string keyID)
+    public bool HasKey(string keyID) // check if the player has a key
     {
         return keys.Contains(keyID);
     }
 
-    public void RemoveKey(string keyID)
+    public void RemoveKey(string keyID) // remove a key from the player's inventory
     {
         keys.Remove(keyID);
     }

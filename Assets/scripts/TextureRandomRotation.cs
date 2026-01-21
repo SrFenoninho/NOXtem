6bebad666c8dbab14a83dd084a95a7a94060618e
SrFenoninho <joaogdmr@gmail.com>
2026-01-21 17:39:07 +0000

    Add Floor1 scene and migrate map1 assets to mapF1

--- Assets/scripts/TextureRandomRotation.cs
+++ Assets/scripts/TextureRandomRotation.cs
@@ -9,17 +9,12 @@ public class TextureRandomRotation : MonoBehaviour
 
         Material mat = renderer.material;
 
-        int rotations = Random.Range(0, 3);
-        float angle = rotations * 90f;
-
-        float rad = angle * Mathf.Deg2Rad;
-
-        Vector2 center = new Vector2(0.5f, 0.5f);
+        int rotations = Random.Range(0, 4);
 
         if (rotations == 1) // 90°
         {
             mat.SetTextureScale("_MainTex", new Vector2(1, 1));
-            mat.SetTextureOffset("_MainTex", new Vector2(0, 0)); 
+            mat.SetTextureOffset("_MainTex", new Vector2(0, 0));
         }
         else if (rotations == 2) // 180°
         {
@@ -31,5 +26,14 @@ public class TextureRandomRotation : MonoBehaviour
             mat.SetTextureScale("_MainTex", new Vector2(-1, 1));
             mat.SetTextureOffset("_MainTex", new Vector2(1, 0));
         }
+
+        float scale = Random.Range(0.8f, 1.5f); // Random scale between 0.8 and 1.5
+        Vector2 currentScale = mat.GetTextureScale("_MainTex");
+        mat.SetTextureScale("_MainTex", currentScale * scale);
+
+        float offsetX = Random.Range(-0.5f, 0.5f); // Random offset between -0.5 and 0.5
+        float offsetY = Random.Range(-0.5f, 0.5f);
+        Vector2 currentOffset = mat.GetTextureOffset("_MainTex");
+        mat.SetTextureOffset("_MainTex", currentOffset + new Vector2(offsetX, offsetY));
     }
 }

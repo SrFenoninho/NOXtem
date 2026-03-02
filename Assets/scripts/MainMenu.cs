47a589ce2ac6cf847d89011e58365eeeb2ed709c
SrFenoninho <joaogdmr@gmail.com>
2026-03-02 10:44:44 +0000

    Update MainMenu.cs

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("FirstPerson");
+        SceneManager.LoadScene("Floor1");
     }
 
     public void QuitGame()
